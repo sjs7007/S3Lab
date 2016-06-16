fa4d39d1d377147ebd765fde2b88e56daac30a45
Saravanan SJ <sjs7007@gmail.com>
2016-06-16 12:33:26 -0400

    test with empty script

--- S3LabUploads/newTest.py
+++ S3LabUploads/newTest.py
@@ -1,1 +1,16 @@
-print "this is test"
+# user input : size of image ( width, height) , number of classification categories (nClass) , number of training steps = nTrain
+# learning rate : alpha, zipFileName : 
+
+
+import tensorflow.examples.tutorials.mnist.input_data
+import input_data
+import zipfile
+import sys, json
+import os
+
+print(os.getcwd())
+#change directory to that of script
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+print(os.getcwd())
