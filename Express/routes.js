6107fb9e34f9566bb4ebb338689b90f85cdd1fec
Saravanan SJ <sjs7007@gmail.com>
2016-06-21 19:45:14 -0400

    User can upload own models and test

--- Express/routes.js
+++ Express/routes.js
@@ -5,6 +5,8 @@ var formidable=require("formidable");
 var path= require("path");
 var http = require('http');
 var modelID = 0;
+var util = require('util');
+
 
 
 // middleware that is specific to this router
@@ -39,6 +41,94 @@ router.get("/MNISTPredictorPage",function (request,response) {
 	    response.end();  
     });
 });
+
+router.get("/generalPredictorPage",function(request,response) {
+	console.log("Request handler 'generalPredictorPage' was called."); 
+
+	fs.readFile('./generalPredictor.html', function (err, html) {
+	    if (err) {
+	        throw err; 
+    	}       
+	    response.writeHead(200, {"Content-Type": "text/html"});  
+	    response.write(html);  
+	    response.end();  
+    });
+
+});
+
+router.post("/generalPredictorModelUpload",function(request,response) {
+	console.log("Request handler for generalPredictorModelUpload called");
+
+	var form = new formidable.IncomingForm();
+	form.keepExtensions = true;
+
+    form.parse(request, function(err, fields, files) {
+	    response.writeHead(200, {'content-type': 'text/plain'});
+	    response.write('received upload:\n\n');
+	    response.end(util.inspect({fields: fields, files: files}));
+	});
+
+	form.on('fileBegin',function(name,file) {
+		file.path="./generalPredictor/"+"general.ckpt";
+	});
+});
+
+router.post("/generalPredictorImageUpload", function(request,response) {
+	console.log("Request handler 'generalPredictorImageUpload' was called.");
+	var form = new formidable.IncomingForm();
+	form.keepExtensions = true; 
+	var data = "";
+
+	form.on('fileBegin', function(name, file) {
+        file.path = "./generalPredictor/"+file.name;
+        data = file.name;
+    });
+
+
+
+	console.log("about to parse.");
+	var dataString = "";
+
+
+	form.parse(request,function(error,fields,files) {
+		console.log("parsing done.");
+		//console.log(fields)
+		//console.log(JSON.stringify(fields))
+		//console.log(files);
+		console.log("File name : "+files.upload.path);
+
+		var spawn = require('child_process').spawn,
+		    py    = spawn('python', ['./generalPredictSavedModel.py'], {cwd:"./generalPredictor"});
+
+		py.stdout.on('data', function(data){
+		  console.log("here1 : "+data);
+		  console.log(data.toString());
+		  dataString += data.toString();
+		});
+
+		console.log("here2");
+
+		py.stderr.on('data', function(data) {
+		  console.log('stdout: ' + data);
+		  dataString += data.toString();
+		});
+
+		py.stdout.on('end', function(){
+			//response.writeHead(200,{"Content-Type":"text/html"});
+			//var p1 = "<html> <img src="+files.upload.path+"> <br>";
+			//var p2 = "Prediction is : "+dataString + "</html>";
+		    //response.write(p2);
+			//response.end();
+			response.setHeader('Content-Type', 'application/json');
+   			response.end(JSON.stringify({ Prediction : dataString.substring(0,dataString.length-1)}));
+		});
+		py.stdin.write(JSON.stringify(data));
+		py.stdin.end();
+			});
+
+});
+
+
 
 
 router.post("/uploadCompleteScript",function (request,response) {
@@ -117,8 +207,9 @@ router.post("/MNISTPredictor", function(request,response) {
 
 	form.parse(request,function(error,fields,files) {
 		console.log("parsing done.");
-		console.log(fields)
-		console.log(JSON.stringify(fields))
+		//console.log(fields)
+		//console.log(JSON.stringify(fields))
+		//console.log(files);
 		console.log("File name : "+files.upload.path);
 
 		var spawn = require('child_process').spawn,
@@ -151,5 +242,6 @@ router.post("/MNISTPredictor", function(request,response) {
 			});
 
 });
+
 
 module.exports = router;
