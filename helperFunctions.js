9db816626d7df9da94a797a2447b992093d794c1
Saravanan SJ <sjs7007@gmail.com>
2016-08-04 01:27:15 -0400

    added emails on status change

--- helperFunctions.js
+++ helperFunctions.js
@@ -1,5 +1,6 @@
 var cassandra = require('cassandra-driver');
 var client = new cassandra.Client({contactPoints: ['127.0.0.1:9042']});
+var fs = require('fs');
 
 module.exports = {
 	//Other helper functions 
@@ -20,5 +21,30 @@ module.exports = {
 		client.execute(query,[job_id],{prepare:true},function(err,res) {
 			return callback(err,res);
 		});
+	},
+
+	sendEmailTo : function(toEmail,message) {
+		fs.readFile('mailgun.key','utf-8',function(err,data) {
+			if(err!=null) {
+				console.log("Mailgun API Key Error : "+err);
+			}
+			else {
+				var api_key = data;
+				var domain = 'sandbox2796510b356d4fd5a02c639af3080887.mailgun.org';
+				var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+				var data = {
+				  from: 'S3 Lab <postmaster@sandbox2796510b356d4fd5a02c639af3080887.mailgun.org>',
+				  to: toEmail ,
+				  subject: 'S3Lab Job Status Update',
+				  text: message
+				};
+				 
+				mailgun.messages().send(data, function (error, body) {
+				  console.log(body);
+				});
+			}
+		});
+
 	}
 }
