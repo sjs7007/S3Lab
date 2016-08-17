a290bf4526f8b621aced3fb8fda849e54695d997
Saravanan SJ <sjs7007@gmail.com>
2016-08-17 17:12:49 -0400

    common template, all buttons done

--- htmlGenerator.js
+++ htmlGenerator.js
@@ -8,7 +8,7 @@ var helper = require('./helperFunctions');
 
 
 module.exports = {
-	getKillButton : function(job_id,pid,url,callback) {
+	/*getKillButton : function(job_id,pid,url,callback) {
 		fs.readFile('./jadeTemplates/killProcess.ejs',function(err,ejsTemplate) {
 			if(err!=null) {
 				helper.logExceptOnTest("Error reading kill template : "+err);
@@ -16,10 +16,37 @@ module.exports = {
 			else {
 				ejsTemplate = ejsTemplate.toString();
 				htmlGen = ejs.render(ejsTemplate,{"job_id" : job_id, "pid" : pid, "url" : url});
-				helper.logExceptOnTest(htmlGen);
-				callback("sjs7007@gmail.com",htmlGen);
+				//helper.logExceptOnTest(htmlGen);
+				callback(htmlGen);
+			}
+		});
+	},
+
+	getSuspendButton : function(job_id,url,callback) {
+		fs.readFile('./jadeTemplates/suspendProcess.ejs', function(err,ejsTemplate) {
+			if(err!=null) {
+				helper.logExceptOnTest("Error reading suspend template : "+err);
+			}
+			else {
+				ejsTemplate = ejsTemplate.toString();
+				htmlGen  = ejs.render(ejsTemplate,{"job_id" : job_id, "url" : url});
+				callback(htmlGen);
+			}
+		});
+	},*/
+
+	getButton : function(job_id,url,buttonText,callback) {
+		fs.readFile('./jadeTemplates/button.ejs', function(err,ejsTemplate) {
+			if(err!=null) {
+				helper.logExceptOnTest("Error reading button template : "+err);
+			}
+			else {
+				ejsTemplate = ejsTemplate.toString();
+				htmlGen  = ejs.render(ejsTemplate,{"job_id" : job_id, "url" : url, "buttonText" : buttonText});
+				callback(htmlGen);
 			}
 		});
 	}
+
 }
 
