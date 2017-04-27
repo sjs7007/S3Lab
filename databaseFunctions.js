361545bdf941b32ccb04c1dd25085fffefc544f1
Saravanan Setty <sjs7007@gmail.com>
2017-04-27 13:20:49 -0400

    changed db name

--- databaseFunctions.js
+++ databaseFunctions.js
@@ -6,7 +6,7 @@ var cassandra = require('cassandra-driver');
 var client = new cassandra.Client({contactPoints: ['127.0.0.1:9042']});
 var helper = require('./helperFunctions');
 //https://www.npmjs.com/package/hashset-native
-var url = "http://localhost:8888";
+var url = "http://localhost:8889";
 var htmlGen = require('./htmlGenerator')
 
 
@@ -17,10 +17,10 @@ module.exports = {
 	// use Prepared statements : http://datastax.github.io/nodejs-driver/getting-started/
 	onDatabaseStart : function() {
 		helper.logExceptOnTest("Clearing up Database...");
-		var query = " SELECT job_id FROM dummyDB.jobInfo  WHERE jobstatus='live' ALLOW FILTERING;";
+		var query = " SELECT job_id FROM DeepCloud.jobInfo  WHERE jobstatus='live' ALLOW FILTERING;";
 		client.execute(query, { prepare: true },function(err,result) {
 			for(var i=0;i<result.rows.length;i++) {
-				var query2= "UPDATE dummyDB.jobInfo SET jobstatus='crashed',pid='processCrashed' WHERE job_id=?;"; 
+				var query2= "UPDATE DeepCloud.jobInfo SET jobstatus='crashed',pid='processCrashed' WHERE job_id=?;"; 
 				client.execute(query2,[result.rows[i].job_id],  { prepare: true },function(err2,result2) {
 					helper.logExceptOnTest("onDatabaseStart Error : "+err2);
 				});
@@ -31,7 +31,7 @@ module.exports = {
 	onProcessSucessDB : function(accuracyValue,modelPath, UUID, pid) {
 		helper.sendEmailTo('sjs7007@gmail.com','Process state of job_id : '+UUID+" changed to "+"processFinished");
 		//processIDSet.remove(pid);
-		var query = "UPDATE dummyDB.jobInfo SET jobStatus='finished',pid='processFinished',accuracy=? ,model=? WHERE job_id=?";
+		var query = "UPDATE DeepCloud.jobInfo SET jobStatus='finished',pid='processFinished',accuracy=? ,model=? WHERE job_id=?";
 		client.execute(query, [accuracyValue,modelPath,UUID], { prepare: true },function(err, result) {
 			helper.logExceptOnTest("onProcessSucessDB Error : "+err);
 		});
@@ -40,7 +40,7 @@ module.exports = {
 	onProcessFailDB : function(accuracyValue,modelPath, UUID, pid) {
 		helper.sendEmailTo('sjs7007@gmail.com','Process state of job_id : '+UUID+" changed to "+"processCrashed");
 		//processIDSet.remove(pid);
-		var query = "UPDATE dummyDB.jobInfo SET jobStatus='finished',pid='processCrashed',accuracy=? ,model=? WHERE job_id=?";
+		var query = "UPDATE DeepCloud.jobInfo SET jobStatus='finished',pid='processCrashed',accuracy=? ,model=? WHERE job_id=?";
 		client.execute(query, [accuracyValue,modelPath,UUID], { prepare: true },function(err, result) {
 			helper.logExceptOnTest("onProcessFailDB Error : "+err);
 		});
@@ -58,7 +58,7 @@ module.exports = {
 			});
 		});
 
-		var query = "INSERT INTO dummyDB.jobInfo (user_id,job_id,jobStatus,jobType,pid) VALUES ('sjs7007testing',?,'live','training',?)";   
+		var query = "INSERT INTO DeepCloud.jobInfo (user_id,job_id,jobStatus,jobType,pid) VALUES ('sjs7007testing',?,'live','training',?)";   
 		client.execute(query, [UUID,pid.toString()], { prepare: true },function(err, result) {
 			helper.logExceptOnTest("onJobCreationDB Error : "+err);
 		});
@@ -68,7 +68,7 @@ module.exports = {
 		helper.sendEmailTo('sjs7007@gmail.com','Process state of job_id : '+job_id+" changed to "+"processKilled");
 		//processIDSet.remove(pid);
 		//add code to update db 
-		var query = "UPDATE dummyDb.jobInfo SET jobStatus='killed',pid='processKilled' WHERE job_id=?;";
+		var query = "UPDATE DeepCloud.jobInfo SET jobStatus='killed',pid='processKilled' WHERE job_id=?;";
 		client.execute(query,[job_id],{prepare : true}, function(err,result) {
 			if(err!=null) {
 				helper.logExceptOnTest("onProcessKillDB error : "+err);
@@ -78,7 +78,7 @@ module.exports = {
 
 	onProcessSuspendDB : function(job_id) {
 		helper.sendEmailTo('sjs7007@gmail.com','Process state of job_id: '+job_id+" changed to "+"processSuspended");
-		var query = "UPDATE dummyDb.jobInfo SET jobStatus='suspended' WHERE job_id=?;";
+		var query = "UPDATE DeepCloud.jobInfo SET jobStatus='suspended' WHERE job_id=?;";
 		client.execute(query,[job_id],{prepare : true}, function(err,result) {
 			if(err!=null) {
 				helper.logExceptOnTest("onProcessSuspendDB error : "+err);
@@ -89,7 +89,7 @@ module.exports = {
 
 	onProcessResumeDB : function(job_id) {
 		helper.sendEmailTo('sjs7007@gmail.com','Process state of PID : '+pid+" changed to "+"processResumed");
-		var query = "UPDATE dummyDb.jobInfo SET jobStatus='live' WHERE job_id=?;";
+		var query = "UPDATE DeepCloud.jobInfo SET jobStatus='live' WHERE job_id=?;";
 		client.execute(query,[job_id],{prepare : true}, function(err,result) {
 			if(err!=null) {
 				helper.logExceptOnTest("onProcessResumeDB error : "+err);
@@ -99,7 +99,7 @@ module.exports = {
 
 	///http://stackoverflow.com/questions/23339907/returning-a-value-from-callback-function-in-node-js
 	dashboardPullDB : function(callback) {
-		var query = "SELECT pid,job_id,jobstatus,jobtype,model,accuracy,prediction,user_id FROM dummyDb.jobInfo;";
+		var query = "SELECT pid,job_id,jobstatus,jobtype,model,accuracy,prediction,user_id FROM DeepCloud.jobInfo;";
 		client.execute(query, { prepare: true },function(err,result) {
 			if(err) {
 				helper.logExceptOnTest("dashboardPullDB Error : "+err);
@@ -113,7 +113,7 @@ module.exports = {
 	},
 
 	dashboardPullDBSelective : function(callback,user_id) {
-		var query = "SELECT pid,job_id,jobstatus,jobtype,model,prediction,user_id FROM dummyDb.jobInfo WHERE user_id=? ALLOW FILTERING;";
+		var query = "SELECT pid,job_id,jobstatus,jobtype,model,prediction,user_id FROM DeepCloud.jobInfo WHERE user_id=? ALLOW FILTERING;";
 		client.execute(query,[user_id], { prepare: true },function(err,result) {
 			if(err) {
 				helper.logExceptOnTest("dashboardPullDBSelective  Error : "+err);
@@ -131,7 +131,7 @@ module.exports = {
 	},
 
 	getModelPath : function(job_id,callback) {
-		var query = "SELECT model FROM dummyDB.jobInfo WHERE job_id=? ALLOW FILTERING;";
+		var query = "SELECT model FROM DeepCloud.jobInfo WHERE job_id=? ALLOW FILTERING;";
 		client.execute(query,[job_id], { prepare : true }, function(err,result) {
 			if(err) {
 				helper.logExceptOnTest("getModelPath Error : "+err);
