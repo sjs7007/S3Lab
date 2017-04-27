12984fd1cc7bd599d05b7e9654f7c2f09f53f732
Saravanan <sjs7007@gmail.com>
2017-04-27 12:57:52 -0400

    added model db functions

--- databaseFunctions.js
+++ databaseFunctions.js
@@ -140,6 +140,64 @@ module.exports = {
 				callback(result);
 			}
 		});
+	},
+
+	addModel : function(modelID,description,modelPath,paper_link,tagsCommaSeparated,developer_username,category,citations,callback) {
+		tagsList = tagsCommaSeparated.split(',');
+		var query = "INSERT INTO tensorServing.modelDetails (modelID,description,modelPath,paper_link,tags,published,developer_username,category,citations) VALUES (?,?,?,?,?,?,?,?,?)";
+		client.execute(query,[modelID,description,modelPath,paper_link,tagsList,false,developer_username,category,citations],{ prepare: true}, function(err,result) {
+			/*if(err!=null) {
+				console.log("error in addModel : "+err);
+			}*/
+			callback(err,result);		
+		});
+	},
+
+	getModelByID : function(modelID,callback) {
+		console.log("getModelByID called.");
+		var query = "SELECT * FROM tensorServing.modelDetails  WHERE modelID=?";
+		client.execute(query,[modelID],{ prepare: true}, function(err,result) {
+			callback(err,result);			
+		});
+	},
+
+	getModelByCategory : function(category,callback) {
+		console.log("getModelByCategory called.");
+		var query = "SELECT * FROM tensorServing.modelDetails  WHERE category=? ALLOW FILTERING";
+		client.execute(query,[category],{ prepare: true}, function(err,result) {
+			callback(err,result);			
+		});
+	},
+
+	getAllModels : function(callback) 
+	{
+		console.log("getAllModels called.");
+		var query = "SELECT * FROM tensorServing.modelDetails";
+		client.execute(query,[],{ prepare: true}, function(err,result) {
+			callback(err,result);			
+		});
+	},
+
+
+	addReview : function(modelID,reviewer_username,review,rating,callback) {
+		var query = "INSERT INTO tensorServing.modelReviews (modelID, reviewer_username, review, rating, timestamp) VALUES (?,?,?,?,?)";
+		client.execute(query,[modelID,reviewer_username,review,rating,cassandra.types.timeuuid()], {prepare: true}, function(err,result) {
+			callback(err,result);
+		});
+	},
+
+	getReviews: function(modelID,callback) {
+		var query = "SELECT * from tensorServing.modelReviews where modelID=?";
+		client.execute(query,[modelID],{prepare : true},function(err,result) {
+			callback(err,result);
+		});
+	},
+
+	getModelRating: function(modelID,callback) {
+		var query = "SELECT AVG(rating) from tensorServing.modelReviews where modelID=?";
+		client.execute(query,[modelID],{prepare : true},function(err,result) {
+			callback(err,result);
+		});
 	}
 
 	/*dashboardPullDB(function(result) {
